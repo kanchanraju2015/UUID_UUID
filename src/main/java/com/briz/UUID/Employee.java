4739d6ca753ba52e5b8a08c195136802f55923f4
kanchan raju <kanchanraju2015@gmail.com>
2023-08-13 15:35:22 +0530

    this is 3rd commit

--- src/main/java/com/briz/UUID/Employee.java
+++ src/main/java/com/briz/UUID/Employee.java
@@ -7,7 +7,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
-@Entity
+@Entity  // the table name will be employee as the name is not provided 
 public class Employee
 {
 	 @Id
